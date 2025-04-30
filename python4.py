80311ce37c7a49ebe10eef9f28043637ca9a71a8
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-04-30 16:38:28 +0545

    Create python4.py

---
+++ python4.py
@@ -0,0 +1,5 @@
+#Input 2 int num a and b and print true if a is greater or equal to b. If not print false 
+
+a=int(input("Enter the value of a:"))
+b=int(input("ENter the value of b:"))
+print("True"if  a>=b else "False")
