09f25d9f7a62350410b31ac72548257f6fbc8a5b
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-04-30 16:23:47 +0545

    Create python3.py

---
+++ python3.py
@@ -0,0 +1,7 @@
+#INput two floating point and print their average
+
+a=float(input("Enter the value of a:"))
+b=float(input("Enter the value of b:"))
+sum=a+b
+Average=sum/2
+print("The average value of given two numbers is", Average)
