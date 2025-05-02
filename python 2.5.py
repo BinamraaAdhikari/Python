13c9ce4a38ae0ee7333c3d2a7786d6451539bec8
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-05-02 20:27:13 +0545

    Create python 2.5.py

---
+++ python 2.5.py
@@ -0,0 +1,9 @@
+#Check if a number is a multiple of 7
+
+
+a=float(input("Enter a number:"))
+b=a%7
+if(b==0):
+    print("The given number is a multiple of 7")
+else:
+    print("The given number is not a multiple of 7")
