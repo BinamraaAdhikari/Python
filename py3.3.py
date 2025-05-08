6b7387b03c96281226a63da973174d9d0e58adeb
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-05-08 14:30:44 +0545

    Create py3.3.py

---
+++ py3.3.py
@@ -0,0 +1,10 @@
+#COunt student with grade A and add in tuple
+
+tup=("C","A","D","A","B","B","A")
+print("The total no of student with grade A are:",tup.count("A"))
+
+#Store above values in list and sort from A to D
+
+ lis=["C","A","D","A","B","B","A"]
+lis.sort()
+print(lis)
