09e10634fa9876d5d546b2656a56954f39940ee3
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-05-08 13:36:07 +0545

    Create python day 3.1.py

---
+++ python day 3.1.py
@@ -0,0 +1,8 @@
+# Ask users 3 favorite movies and store them in list
+
+
+print("Enter the name of your 3 favorite movies")
+a=input("Enter the name of your 3rd favorite movie:")
+b=input("Enter the name of your 2nd favorite movie:")
+c=input("Enter the name of your most favorite movie:")
+print("Your favorite movies in order is",[c,b,a])
