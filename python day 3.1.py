291e7b9ce79df82c045de2b592765fd006e212df
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-05-08 13:50:59 +0545

    Update python day 3.1.py

--- python day 3.1.py
+++ python day 3.1.py
@@ -1,8 +1,9 @@
 # Ask users 3 favorite movies and store them in list
 
 
-print("Enter the name of your 3 favorite movies")
-a=input("Enter the name of your 3rd favorite movie:")
-b=input("Enter the name of your 2nd favorite movie:")
-c=input("Enter the name of your most favorite movie:")
-print("Your favorite movies in order is",[c,b,a])
+movies = []
+movies.append(input("Enter your 1st favorite movie: "))
+movies.append(input("Enter your 2nd favorite movie: "))
+movies.append(input("Enter your 3rd favorite movie: "))
+print("Your favourite movies are:",",".join(movies))
+
